0309a5c764f22c130110d8489349f71f0f37f3a6
Sierra Portlock <portlock.sierra@gmail.com>
2020-08-08 10:53:13 -0400

    test push

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # TriviaGame
 HW 5 | JS Assignment 2
+
+Questions
+
+How many members are in Umbrella Academy?
+Who is children's father?
+Which member started a cult?
+Which president was Diego obsessed with rescuing?
+What is the name of the secret society that protects the timeline?
---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+// hide quiz questions until begin quiz is selected. **********
+
+// - Select bigin game *************
+// - display div with questions and submit button when select begin game
+// - begin countdown
+
+// When timer runs out OR user hits submit ******
+
+// - display out of time 
+// - mark unanswered questions as incorrect 
+// - display correct answers
+// - display number of questions answered correctly
+
+// begin new game button to display different questions *bonus
