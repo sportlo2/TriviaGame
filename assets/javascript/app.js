e3389b4a961ecad5257d893bc160e97fb8c3a806
Sierra Portlock <portlock.sierra@gmail.com>
2020-08-22 22:23:29 -0400

    add timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,84 +13,111 @@
 
 // begin new game button to display different questions *bonus
 
-// ** coding begins HERE
+// ** coding begins HERE*******************************
 
 // variables to hold HTML elemesnts
-
 var quizContainer = $("#quiz");
-var submitBtn = $("#submit");
 var resultsContainer = $("#results");
 var startBtn = $("#startQuiz");
+
+
+// questions var
+var quizQuestions = [
+    {
+        question: "How many members are in Umbrella Academy?",
+        answers: ["a: 5", "b: 7", "c: 9"],
+        correctAnswer: "b: 7"
+    },
+    {
+        question: "Who is the children's father?",
+        answers: ["a: Sir Reginald Hargreeves", "b: Sir Jim Ratcliff", "c: Sir Rap-A-Lot"],
+        correctAnswer: "a: Sir Reginald Hargreeves"
+    },
+    {
+        question: "Which member started a cult?",
+        answers: [
+            "a: Klaus",
+            "b: Diego",
+            "c: Allison"],
+        correctAnswer: "a: Klaus"
+    },
+    {
+        question: "Which president was Diego obsessed with rescuing?",
+        answers: [
+            "a: Lincoln",
+            "b: Obama",
+            "c: Kennedy"
+        ],
+        correctAnswer: "c: Kennedy"
+    },
+    {
+        question: "What is the name of the secret society that protects the timeline?",
+        answers: [
+            "a: Area 51",
+            "b: The Commission",
+            "c: Preservation Society"
+        ],
+        correctAnswer: "b: The Commission"
+    },
+]
 // create functions
+//call function to start timer here. or put function here then call it
+var counter = 60;
+
+
+
 
 //build quiz function use function(); to display quiz
-function buildQuiz(){
+function buildQuiz() {
+    //Timer goes here. prepend timer and add attributes
+    startBtn.remove();
 
+    let Timer = setInterval(function () {
+        counter--;
+        $("#countdown").html("<h2>Timer:" + counter);
+        if (counter === 0) {
+            console.log(done);
+            clearInterval(Timer);
+            showResults();
+        }
+    }, 1000);
+
+    //$("#countdown").html(Timer);
+
+
+
+    for (var i = 0; i < quizQuestions.length; i++) {
+        quizContainer.append('<h3>' + quizQuestions[i].question + '</h3>');
+        for (var j = 0; j < quizQuestions[i].answers.length; j++) {
+            quizContainer.append("<input type='radio' name='question-" + i +
+                "' value='" + quizQuestions[i].answers[j] + "''>" + quizQuestions[i].answers[j]);
+        }
+    }
+    //display submit button once click begin button
+    //add attribute to center button
+    $("#results").html("<button id='submit'>Submit</button>");
+
+    $("#submit").click(function () {
+        console.log("this button works!");
+    });
 }
 
- // begin quiz when select button
+// begin quiz when select button
 startBtn.click(buildQuiz);
+
 
 //results show when user selects submit or timer runs out
 function showResults() {
 
 }
-
 // show results when hit submit button
-submitBtn.click(showResults);
+// submitBtn.click(showResults);
 
-// show results when timer runs out
+// $("#submit").on("click", function(){
+//     console.log("this button works!");
+// })
 
-// questions var
 
-var questions = [
-    {
-        question: "How many members are in Umbrella Academy?",
-        answers: {
-        a: "5",
-        b: "7",
-        c: "9"
-        },
-        correctAnswer: "b"
-    },
+// // show results when timer runs out
 
-    {
-        question: "Who is the children's father?",
-        answers: {
-        a: "Sir Reginald Hargreeves",
-        b: "Sir Jim Ratcliff",
-        c: "Sir Rap-A-Lot"
-        },
-        correctAnswer: "a"
-    },
-
-    {
-        question: "Which member started a cult?",
-        answers: {
-        a: "Klaus",
-        b: "Diego",
-        c: "Allison"
-        },
-        correctAnswer: "a"
-    },
-
-    {
-        question: "Which president was Diego obsessed with rescuing?",
-        answers: {
-        a: "Lincoln",
-        b: "Obama",
-        c: "Kennedy"
-        },
-        correctAnswer: "c"
-    },
-
-    {
-        question: "What is the name of the secret society that protects the timeline?",
-        answers: {
-        a: "Area 51",
-        b: "The Commission",
-        c: "Preservation Society"
-        },
-        correctAnswer: "b"
-    },
-]
+// var endGame
